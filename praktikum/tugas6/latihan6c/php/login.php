c409a6806c3c096d7790c526d9e6b0bb7a0a1837
FaisalHakim081 <faisalhakim081@gmail.com>
2021-04-25 22:18:47 +0700

    menyelesaikan tugas praktikum modul 6

--- praktikum/tugas6/latihan6c/php/login.php
+++ praktikum/tugas6/latihan6c/php/login.php
@@ -51,29 +51,33 @@ if (isset($_POST['submit'])) {
   $error = true;
 }
 ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-<form action="" method="POST">
+<form action="" method="POST" style="margin-left: 25px;">
   <?php if (isset($error)) : ?>
     <p style="color: red; font-style:italic;">Username dan Password salah</p>
   <?php endif; ?>
-  <table>
+  <table style="margin-left: 50px;">
     <tr>
-      <td><label for="username">Username</label></td>
+      <h2 style="color: turquoise;">Masukan Username dan Passsword</h2>
+    </tr>
+    <tr>
+      <td><label for="username"><span class="badge bg-success">Username</span></label></td>
       <td>:</td>
       <td><input type="text" name="username"></td>
     </tr>
     <tr>
-      <td><label for="password">Password</label></td>
+      <td><label for="password"><span class="badge bg-success">Password</span></label></td>
       <td>:</td>
       <td><input type="password" name="password"></td>
     </tr>
   </table>
   <div class="remember">
     <input type="checkbox" name="remember">
-    <label for="remember">Remember me</label>
+    <label for="remember"><span style="font-weight: bold;">Remember me</span></label>
   </div>
-  <button type="submit" name="submit">Login</button>
+  <button style="margin-top: 15px;margin-left: 55px;" class="btn btn-primary" type="submit" name="submit">Login</button>
   <div class="registrasi">
-    <p>Belum punya akun ? Registrasi <a href="registrasi.php">Disini</a></p>
+    <p style="font-weight: bold;margin-top:15px;">Belum punya akun ? Registrasi <a href="registrasi.php">Disini</a></p>
   </div>
 </form>
--- praktikum/tugas6/latihan6c/php/registrasi.php
+++ praktikum/tugas6/latihan6c/php/registrasi.php
@@ -14,19 +14,26 @@ if (isset($_POST["register"])) {
   }
 }
 ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-<form action="" method="POST">
-  <table>
+<form action="" method="POST" style="margin-left: 25px;">
+  <table style="margin-left: 50px;">
     <tr>
-      <td><label for="username">Username</label></td>
+      <h2 style="color: turquoise;">Buatlah Username dan Passsword</h2>
+    </tr>
+    <tr>
+      <td><label for="username"><span class="badge bg-success">Username</span></label></td>
       <td>:</td>
       <td><input type="text" name="username"></td>
     </tr>
     <tr>
-      <td><label for="password">Password</label></td>
+      <td><label for="password"><span class="badge bg-success">Password</span></label></td>
       <td>:</td>
       <td><input type="password" name="password"></td>
     </tr>
   </table>
-  <button type="submit" name="register">REGISTER</button>
+  <button type="submit" name="register" style="margin-top: 15px;margin-left: 55px;" class="btn btn-primary">REGISTER</button>
+  <div class="login">
+    <p style="font-weight: bold;margin-top:15px;">Sudah punya akun ? Login <a href="login.php">Disini</a></p>
+  </div>
 </form>
